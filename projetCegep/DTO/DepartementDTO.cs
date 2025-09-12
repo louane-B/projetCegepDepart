56a55d80065280a016e8fdd11f4f66addc483449
louane-B <louane31012004@gmail.com>
2025-09-12 10:36:29 -0400

    création DépartementDTO

--- projetCegep/DTO/DepartementDTO.cs
+++ projetCegep/DTO/DepartementDTO.cs
@@ -7,6 +7,25 @@ namespace ProjetCegep.DTO
         public string Nom { get; set; }
         public string Description { get; set; }
 
-        
+        //constructeur par défaut
+        public DepartementDTO(string no = "", string nom = "", string description = "")
+        {
+            No = no;
+            Nom = nom;
+            Description = description;
+        }
+
+        //constructeur a partir d'un objet Departement
+        public DepartementDTO(Departement monDepartement)
+        {
+            No = monDepartement.No;
+            Nom = monDepartement.Nom;
+            Description = monDepartement.Description;
+        }
+
+        public override string ToString()
+        {
+            return $"{No}\n{Nom}\n{Description}";
+        }
     }
 }
