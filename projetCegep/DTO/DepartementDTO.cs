35a67342a6b1198bc51a5ab48742801ffd7fabbc
louane-B <louane31012004@gmail.com>
2025-09-12 08:33:23 -0400

    Ajout de DepartementDTO et EnseignementDTO

---
+++ projetCegep/DTO/DepartementDTO.cs
@@ -0,0 +1,12 @@
+namespace ProjetCegep.DTO
+{
+    public class DepartementDTO
+    {
+        //propriété publique
+        public string No { get; set; }
+        public string Nom { get; set; }
+        public string Description { get; set; }
+
+        
+    }
+}
---
+++ projetCegep/DTO/EnseignementDTO.cs
@@ -0,0 +1,18 @@
+namespace ProjetCegep.DTO
+{
+    public class EnseignementDTO
+    {
+        public int NoEmploye { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Adresse { get; set; }
+        public string Ville { get; set; }
+        public string Province { get; set; }
+        public string CodePostal { get; set; }
+        public string Telephone { get; set; }
+        public string Courriel { get; set; }
+        public string DateEmbauche { get; set; }
+        public string DateArret { get; set; }
+
+    }
+}
